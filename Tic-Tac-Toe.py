ea26371da39d618bb5fbbeef2b318d83737fae53
M.Dallman <matthew.dallman@ntlworld.com>
2019-10-27 17:18:18 +0000

    Create Tic-Tac-Toe.py

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,11 @@
+def play_game():
+    ###
+    #Ask if One or Two players
+    #Then
+    #Display Board
+    #Get location of player input
+    #Check if location has been taken
+    # if it has ask for new input
+    #if it hasn't then assign that location to player x
+    #Change the players turn.
+    ###
