0241e779d750fb1576c3541191c6c17829d130e2
M.Dallman <matthew.dallman@ntlworld.com>
2019-10-27 18:11:33 +0000

    Update Tic-Tac-Toe.py
    Created function to set variables back to default when a new game starts.

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -9,26 +9,10 @@ def play_game():
     #if it hasn't then assign that location to player x
     #Change the players turn.
     ###
-
-    #Creating the Variables for the squares.
-    a1 = 'a'
-    a2 = 'a'
-    a3 = 'a'
-    b1 = 'a'
-    b2 = 'a'
-    b3 = 'a'
-    c1 = 'a'
-    c2 = 'a'
-    c3 = 'a'
-
-    #Sets second player to false at start.
-    second_player = False
-
-    #Sets the players turn to player 1
-    player_turn = 1
-        
+    new_game_variables()    
     welcome()
     number_of_players()
+    display_board()
    
 
 def welcome():
@@ -37,12 +21,20 @@ def welcome():
 X O X O X O X O
 X Tic-Tac-Toe O
 X O X O X O X O
-
-    
+ 
     ''')
     
 def display_board():
 #Displays the board.
+    global a1
+    global a2
+    global a3
+    global b1
+    global b2
+    global b3
+    global c1
+    global c2
+    global c3
     print('''
 
    | 1 | 2 | 3 |
@@ -74,10 +66,8 @@ def number_of_players():
     #Gets the number of players and sets the second_player variable
     global second_player
     x = input('Please enter how many players, 1 or 2:')
-    print(x)
     if x == '2':
         second_player = True
-        print('There are 2 players')
     else:
         second_player = False
     
@@ -97,7 +87,55 @@ def computer_turn():
 def check_win():
     pass
 
+def new_game_variables():
+    #Sets the variables back to default for a new game.
+    #Sets grid variables back to default
+    global a1
+    global a2
+    global a3
+    global b1
+    global b2
+    global b3
+    global c1
+    global c2
+    global c3
+
+    a1 = 'a'
+    a2 = 'a'
+    a3 = 'a'
+    b1 = 'a'
+    b2 = 'a'
+    b3 = 'a'
+    c1 = 'a'
+    c2 = 'a'
+    c3 = 'a'
+    
+    #Sets second player to false at start.
+    global second_player
+    second_player = False
+    
+    #Sets the players turn to player 1
+    global player_turn
+    player_turn = 1
 
 
 
+
+    
+#creates variables for the squares.
+a1 = 'a'
+a2 = 'a'
+a3 = 'a'
+b1 = 'a'
+b2 = 'a'
+b3 = 'a'
+c1 = 'a'
+c2 = 'a'
+c3 = 'a'
+
+#Sets second player to false at start.
+second_player = False
+
+#Sets the players turn to player 1
+player_turn = 1
 
